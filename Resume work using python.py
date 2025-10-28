8d003289d693cd68a3a925882ce47d8ac6a14036
pavithrapalani-95 <pmgpavithra@gmail.com>
2025-10-28 17:04:15 +0530

    file upload

---
+++ Resume work using python.py
@@ -0,0 +1,12 @@
+print("RESUME")
+print("Name : Pavithra")
+print("D.O.B : 21/02/1995")
+print("Gender : Female")
+print("Educational Qualification : BE (E&I), 2016")
+print("Work Experience : worked as Instrument Design Engineer in Chennai for 2 years")
+print("Job Details : Preparing list of deliverables")
+print("Father name : N. Palanichamy")
+print("Mother name : G. Muniammal")
+print("Place of Residence : Kovilpatti")
+
+
