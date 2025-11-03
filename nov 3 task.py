954d05c0fa5a5c232decc7b361669c88ba0f25cd
pavithrapalani-95 <pmgpavithra@gmail.com>
2025-11-03 18:04:48 +0530

    Add files via upload

---
+++ nov 3 task.py
@@ -0,0 +1,65 @@
+print("1. numbers from 1 to 20")
+
+
+for i in range(1,21):
+        print(i)
+    
+
+print("2. even numbers from 2 to 50")
+
+
+for i in range(2,51,2):
+    print(i)
+    
+
+print("3. odd numbers from 1 to 50")
+
+
+for i in range(1,51,2):
+    print(i)
+
+
+
+print("4. square of numbers from 1 to 15")
+
+
+for i in range(1,16):
+    print(i**2)
+
+
+print("5. cube of numbers from 1 to 10")
+
+for i in range(1,11):
+    print(i**3)
+
+
+print("6. reverse order from 10 to 1")
+
+for i in range(10,0,-1):
+      print(i)
+
+
+print("7. multiplication table 5")
+
+
+for i in range(1,11):
+    print(f"{i}*5={i*5}")
+
+
+print("8. print characters in string one by one")
+
+
+for i in ("Apple"):
+    print(i)
+
+
+
+print("9. numbers divisible by 3 btw 1 and 30")
+
+
+for i in range(1,31):
+    if(i%3==0):
+        print(i)
+
+
+    
